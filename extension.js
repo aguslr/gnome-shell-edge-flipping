cc3dc4a78b59589c1321d327eb6a71a680155557
Meng ZHuo <mengzhuo1203@gmail.com>
2012-10-01 15:44:20 +0800

    update for 3.6 still, it has bugs like: can't find this._settings

--- extension.js
+++ extension.js
@@ -23,19 +23,22 @@ const Lang = imports.lang;
 const Main = imports.ui.main
 const Mainloop = imports.mainloop;
 const WorkspaceSwitcherPopup = imports.ui.workspaceSwitcherPopup;
+const Meta = imports.gi.Meta
 
 const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
 const Convenience = Me.imports.convenience;
 
-function EdgeFlipping() {
-    this._init();
-}
-
 // Object structure
-EdgeFlipping.prototype = {
+const EdgeFlipping = new Lang.Class({
+
+    Name: 'EdgeFlipping',
+    
     _init: function() {
+    
         this._settings = Convenience.getSettings();
+        
+        global._edge = this
 
         // Calculate some variables
         this._monitor = Main.layoutManager.primaryMonitor;
@@ -83,7 +86,7 @@ EdgeFlipping.prototype = {
             this._edges[edge].connect ('enter-event', Lang.bind (this, this._switchWorkspace));
             this._edges[edge].connect ('leave-event', Lang.bind (this, this._removeTimeout));
             this._edges[edge].opacity = this._settings.get_int("opacity");
-            Main.layoutManager.addChrome (this._edges[edge], { visibleInFullscreen:true });
+            Main.layoutManager.addChrome (this._edges[edge], { trackFullscreen: true });
         };
 
         // When display setup changes, recreate edges
@@ -137,16 +140,16 @@ EdgeFlipping.prototype = {
         this._initialDelayTimeoutId = Mainloop.timeout_add (this._settings.get_int("delay-timeout"), function() {
             switch (actor.name) {
                 case "top-edge":
-                    Main.wm.actionMoveWorkspaceUp();
+                    Main.wm.actionMoveWorkspace(Meta.MotionDirection.UP);
                     break;
                 case "bottom-edge":
-                    Main.wm.actionMoveWorkspaceDown();
+                    Main.wm.actionMoveWorkspace(Meta.MotionDirection.DOWN);
                     break;
                 case "right-edge":
-                    Main.wm.actionMoveWorkspaceRight();
+                    Main.wm.actionMoveWorkspace(Meta.MotionDirection.RIGHT);
                     break;
                 case "left-edge":
-                    Main.wm.actionMoveWorkspaceLeft();
+                    Main.wm.actionMoveWorkspace(Meta.MotionDirection.LEFT);
                     break;
             };
             // Check if we are in the last workspace on either end
@@ -192,7 +195,7 @@ EdgeFlipping.prototype = {
             this._edges[edge].destroy();
         }
     }
-}
+})
 
 function init() {
     if (Main.wm._workspaceSwitcherPopup == null)
