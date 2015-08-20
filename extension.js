863af7ff6545c88f2015b78f894bbfb2e9f0156e
Agus Lopez <contact@aguslr.com>
2015-08-20 14:04:50 +0200

    extension.js: Change default email address

--- extension.js
+++ extension.js
@@ -1,7 +1,7 @@
 /* -*- mode: js2; js2-basic-offset: 4; indent-tabs-mode: nil -*- */
 /*
  * edge-fipping
- * Copyright (C) 2012 Agus Lopez <aremuinan@gmail.com>,
+ * Copyright (C) 2012 Agus Lopez <contact@aguslr.com>,
  * Christian Schramm <christian.h.m.schramm@gmail.com>
  *
  * edge-flipping is free software: you can redistribute it and/or modify it
